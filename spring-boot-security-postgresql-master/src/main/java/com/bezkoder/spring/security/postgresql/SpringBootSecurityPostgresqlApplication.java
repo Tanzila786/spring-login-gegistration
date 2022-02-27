4c6cb5a875dc10d2776277e29058aca11199a53d
tanzil786 <tanzilamulla97@gmail.com>
2022-02-27 22:28:28 +0530

    Intial Project

---
+++ spring-boot-security-postgresql-master/src/main/java/com/bezkoder/spring/security/postgresql/SpringBootSecurityPostgresqlApplication.java
@@ -0,0 +1,15 @@
+package com.bezkoder.spring.security.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SpringBootConfiguration
+public class SpringBootSecurityPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityPostgresqlApplication.class, args);
+	}
+
+}
